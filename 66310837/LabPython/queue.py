3bce14c47be045a6e25bed2ff59e42128bc5c5ba
BeelzebubCode <pongrock1234@gmail.com>
2024-08-08 14:45:40 +0700

    data sci

---
+++ 66310837/LabPython/queue.py
@@ -0,0 +1,17 @@
+from implementing import Queue
+
+def hotPotato(namelist, num):
+    simqueue = Queue()
+    for name in namelist:
+        simqueue.enqueue(name)
+    # print(simqueue.items)
+    while simqueue.size() > 1:
+        for _ in range(num):
+            simqueue.enqueue(simqueue.dequeue())
+
+        # print(simqueue.items)
+        simqueue.dequeue()
+
+    return simqueue.dequeue()
+
+print(hotPotato(["Bill", "David", "Susan", "Jane", "Kent", "Brad"], 7))
---
+++ 66310837/LabPython/stack.py
@@ -0,0 +1,19 @@
+# from implementing import Stack
+
+# def divideBy2(decNumber):
+#     remstack = Stack()
+
+#     while decNumber > 0:
+#         rem = decNumber % 2
+#         remstack.push(rem)
+#         decNumber = decNumber // 2
+
+#     bitString = ""
+#     while not remstack.isEmpty():
+#         bitString+=str(remstack.pop())
+
+#     return bitString
+
+# print(divideBy2(42))
+############################################################
+
