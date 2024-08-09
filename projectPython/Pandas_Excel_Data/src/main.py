36a124f450a41b252f68c6ea9c6d6a0d5d49163e
BeelzebubCode <pongrock1234@gmail.com>
2024-08-09 15:07:22 +0700

    new commit

--- projectPython/Pandas_Excel_Data/src/main.py
+++ projectPython/Pandas_Excel_Data/src/main.py
@@ -12,18 +12,19 @@ with open("data.csv", encoding='utf-8') as file:
 
 data = sorted(data, key=lambda data : data["โรงเรียน"])
 
-i = 0
+i = 1
 for item in data:
     if i % 2 == 0:
         group_a.append(item)
     else:
         group_b.append(item)
     i+=1
+# print(len(group_a), len(group_b))
 
 # สร้าง DataFrame 
 df_group_a = pd.DataFrame(group_a, columns=["โรงเรียน", "จังหวัด", "ทีม"])
 df_group_b = pd.DataFrame(group_b, columns=["โรงเรียน", "จังหวัด", "ทีม"])
-
+# print(len(df_group_a), len(df_group_b))
 with pd.ExcelWriter('rov_group.xlsx', engine='openpyxl') as writer:
     df_group_a.to_excel(writer, sheet_name='Groups', startrow=2, index=False, header=False)
     df_group_b.to_excel(writer, sheet_name='Groups', startrow=len(df_group_a)+4, index=False, header=False)
@@ -37,10 +38,10 @@ with pd.ExcelWriter('rov_group.xlsx', engine='openpyxl') as writer:
 
     # เขียนหัวข้อ Group B
     start_row_group_b = len(df_group_a) + 5
-    worksheet.cell(row=start_row_group_b - 1, column=1, value='Group B')
-    worksheet.cell(row=start_row_group_b, column=1, value='โรงเรียน')
-    worksheet.cell(row=start_row_group_b, column=2, value='จังหวัด')
-    worksheet.cell(row=start_row_group_b, column=3, value='ทีม')
+    worksheet.cell(row=start_row_group_b-2, column=1, value='Group B')
+    worksheet.cell(row=start_row_group_b-1, column=1, value='โรงเรียน')
+    worksheet.cell(row=start_row_group_b-1, column=2, value='จังหวัด')
+    worksheet.cell(row=start_row_group_b-1, column=3, value='ทีม')
 
     for col in worksheet.columns:
         max_length = 0
