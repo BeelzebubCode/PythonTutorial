33409d76fe721430f1add8a8669578eb70acdc40
BeelzebubCode <pongrock1234@gmail.com>
2024-08-12 22:50:27 +0700

    commit

--- 66310837/python/Quiz/sorted_number.py
+++ 66310837/python/Quiz/sorted_number.py
@@ -1,4 +1,6 @@
 number = sorted(list(map(int, input().split())), reverse=True)
+# reverse = True ทำให้เรียงจากมากไปน้อย
+
 # number = input().split()
 # number = list(map(int, number))
 # number = sorted(number, reverse=True)
@@ -6,5 +8,6 @@ number = sorted(list(map(int, input().split())), reverse=True)
 if len(number) < 5:
     print("Too few input")
 else:
+    # print() 3  index แรกมาเลยเนื่องจาก sorted มาก->น้อย แล้ว
     for i in range(3):
         print(number[i], end=" ")
--- 66310837/python/Quiz/sum_of_number.py
+++ 66310837/python/Quiz/sum_of_number.py
@@ -2,13 +2,18 @@ n = list(map(int, input().split()))
 is_sum = int(input())
 
 index_sum = []
+# -4 เพื่อกัน index เกิน
 for i in range(len(n)-4):
+    # ผลรวมของตำแหน่งปัจจุบัน i + กับ ตำแหน่งถัดไปอีก 4 ตำแหน่ง
     sum_of_five = sum(n[i:i+5])
-    if sum_of_five == is_sum:
+    if sum_of_five == is_sum: 
+        # ถ้าผลรวม = ค่าที่เราต้องการ ให้เพิ่มไปใน list
         index_sum.append(i)
 
+# ถ้าไม่ใช่ list ว่างจะแสดงข้อมูล index
 if index_sum:
     for i in index_sum:
         print(i)
 else:
+    # ถ้าไม่เจอเลยจะปริ้น -1
     print(-1)
