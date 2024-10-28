89cb8843f67df91aa90f707a61f5fddb0d7a4f02
BeelzebubCode <pongrock1234@gmail.com>
2024-10-28 21:29:22 +0700

    new

--- testDebug.py
+++ testDebug.py
@@ -380,29 +380,5 @@
 # else:
 #     print(-1)
 
-n = int(input())
-
-group_1 = []
-for _ in range(n):
-    number = int(input())
-    group_1.append(number)
-
-group_2 = list(map(int, input().split()))
-group_3 = []
-while True:
-    number = int(input())
-    if number == -1: break
-    group_3.append(number)
-
-all_group = [group_1, group_2, group_3]
-result = []
-for i in range(len(all_group)):
-    count = 0
-    for number in all_group[i]:
-        if count % 2 == 0:
-            result.insert(-1, number)
-        else:
-            result.insert(0, number)
-        count += 1
-
-print(result)
+a = 0; b = 20; c = 70
+print(a or b > c)
